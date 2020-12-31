c1ba523c1e4b1007b2b7d6b4f1cf812635f63027
Prashant Jain <prashantjain0501@gmail.com>
2020-12-31 23:20:50 +0530

    Final code after implementation of all the features provided by our app.

---
+++ app/src/main/java/com/example/miwok/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.miwok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Find the views that shows the numbers category
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+
+        //set a clicklistener on that view
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView family = (TextView) findViewById(R.id.family);
+
+        //set a clicklistener on that view
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView colors = (TextView) findViewById(R.id.colors);
+
+        //set a clicklistener on that view
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+
+        //set a clicklistener on that view
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/miwok/WordAdapter.java
@@ -0,0 +1,62 @@
+package com.example.miwok;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+    private int mColorResourceId;
+    private MediaPlayer mediaPlayer;
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId){
+        super(context, 0, words);
+        mColorResourceId = colorResourceId;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+        Word local_word = getItem(position);
+
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        if(local_word.hasImage()) {
+            imageView.setImageResource(local_word.getImageResourceId());
+            //If View is reused and has Image It should be set to visible
+            imageView.setVisibility(View.VISIBLE);
+        }
+        else{
+            //if view doesn't have an image
+            imageView.setVisibility(View.GONE);
+        }
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(local_word.getMiwokTranslation());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(local_word.getDefaultTranslation());
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        textContainer.setBackgroundColor(color);
+
+        return listItemView;
+    }
+}
